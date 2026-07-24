dc6b3425c3db584e1b474fb8c5f4605d3f6c0dd2
kiraplane <zhh0504@gmail.com>
2026-07-24 13:27:20 +0800

    chore: apply July 24 growth updates

--- src/data/cursedcompanions/guides.ts
+++ src/data/cursedcompanions/guides.ts
@@ -1,6 +1,6 @@
 import type { Guide, GuideVideo } from './types';
 
-const checkedAt = '2026-07-21';
+const checkedAt = '2026-07-24';
 
 function youtubeThumbnail(id: string) {
   return `https://i.ytimg.com/vi/${id}/hq720.jpg`;
